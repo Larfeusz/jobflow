4a1c6cc827f2936ee38e5464948f92e4841c3cf1
Larfeusz” <piotr.jakubiak@onet.pl>
2020-09-08 11:11:36 +0200

    first commit

---
+++ src/test/java/com/larfeusz/jobflow/JobflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larfeusz.jobflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
